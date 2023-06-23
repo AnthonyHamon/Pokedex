4da0380f54b9b254fa7b43a1e2f7614ae896c278
AnthonyHamon <anthony.hamon91@gmail.com>
2023-06-23 16:42:45 +0200

    first pokemon image added

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ async function loadPokemon(){
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log('Abgerufene Pokemon ist:', currentPokemon);
+
+    renderPokemonInfo();
 }
 
 function renderPokemonInfo(){
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImg').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
 }
